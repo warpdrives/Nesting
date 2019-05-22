d42de5e6efbde457a857ae4243a7b139cb4595ba
shevakuilin <shevakuilin@gmail.com>
2019-05-22 11:15:19 +0800

    Update add ne_print method

--- Source/Common/NeCommon.swift
+++ Source/Common/NeCommon.swift
@@ -12,14 +12,17 @@ private let NE_FROM_MESSAGE = "[**From Nesting**]"
 private let NE_ASSERT_CHILDCONTROLLERCOUNT_ERROR = "\(NE_FROM_MESSAGE) The nested viewController needs to contain at least two childControllers, you must have the value of childControllerCount at least greater than 1"
 
 public enum NEAssertType: Int {
+    /**
+     * @abstract The count of childController is less than 2.
+     */
     case childControllerCount
 }
 
 /**
  *  Assert
  *
- *  @param type     Type of assert
- *  @param value    Generics that follow the <Comparable> protocol
+ *  @param type     Type of assert.
+ *  @param value    Generics that follow the <Comparable> protocol.
  *
  */
 public func ne_assert<T: Comparable>(type: NEAssertType, value: T) {
@@ -28,4 +31,21 @@ public func ne_assert<T: Comparable>(type: NEAssertType, value: T) {
         let minValue: T = 1 as! T
         assert(value > minValue, NE_ASSERT_CHILDCONTROLLERCOUNT_ERROR)
     }
+}
+
+/** Format print output
+ *
+ *  @param message  Output text message. [e.g., "array.first"]
+ *  @param file     The path to the file containing this symbol.
+ *  @param method   Method name containing this symbol.
+ *  @param line     The line number where the symbol appears.
+ *
+ */
+public func ne_print<T>(_ message: T,
+                        file: String = #file,
+                        method: String = #function,
+                        line: Int = #line) {
+    #if DEBUG
+    print("\n[\((file as NSString).lastPathComponent) \(method)] - [Line \(line)] - \(message)\n")
+    #endif
 }
