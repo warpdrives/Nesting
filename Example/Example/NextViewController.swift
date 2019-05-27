72d0f6aa16430bb5d852f886eb12e384a2171d05
burning_git <burning_git@163.com>
2019-05-27 09:32:54 +0800

    Add cache NeConfig class

---
+++ Example/Example/NextViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NextViewController.swift
+//  Example
+//
+//  Created by git burning on 2019/5/26.
+//  Copyright © 2019 Warpdrives. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white;
+        self.ne_backgroundColor = UIColor.orange;
+        self.ne_creatContainerScrollView(5) // Test: Creat scrollView
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    deinit {
+        self.ne_removeConfig()
+    }
+}
--- Source/Protocol/NeProtocol.swift
+++ Source/Protocol/NeProtocol.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-protocol CreatNested: class {
+protocol CreatNested:class{
     /**
      *  Create a low-level container based on UIScrollView.
      *
@@ -16,10 +16,35 @@ protocol CreatNested: class {
      *
      */
     func ne_creatContainerScrollView(_ childControllerCount: Int)
+    
+    var ne_backgroundColor:UIColor {set get}
+}
+
+public class CreatNestedProtocolModel {
+    
+    var ne_backgroundColor:UIColor = UIColor.red
 }
 
 extension CreatNested where Self: UIViewController {
+    
     func ne_creatContainerScrollView(_ childControllerCount: Int) {
         ne_creatNestedViewController(childControllerCount)
     }
+
+    var ne_backgroundColor: UIColor {
+        get {
+            return self.ne_getCurrentConfig().ne_backgroundColor
+        }
+        set {
+//class
+            self.ne_getCurrentConfig().ne_backgroundColor = newValue
+// struct
+//            var config = self.ne_getCurrentConfig()
+//            config.ne_backgroundColor = newValue
+//            NeConfig.default.ne_nestedDict[self.ne_getCurrentVCAddressPointer()] = config
+//            self.ne_getCurrentConfig().ne_backgroundColor = newValue
+        }
+    }
+
+   
 }
