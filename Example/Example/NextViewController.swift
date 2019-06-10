56b2e51ed42e8da3cb2d3844f2beb7d91623b57b
shevakuilin <shevakuilin@gmail.com>
2019-06-10 17:35:15 +0800

    Modify project organization name & header commit

--- Example/Example/NextViewController.swift
+++ Example/Example/NextViewController.swift
@@ -1,19 +1,10 @@
 //
-//  Nesting
+//  NextViewController.swift
+//  Example
 //
-//  Copyright (c) 2019-Present Warpdrives Team - https://github.com/warpdrives
+//  Created by git burning on 2019/5/26.
+//  Copyright © 2019 Warpdrives Team. All rights reserved.
 //
-//  The software is available under the Apache 2.0 license. See the LICENSE file
-//  for more info.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-
 
 import UIKit
 
