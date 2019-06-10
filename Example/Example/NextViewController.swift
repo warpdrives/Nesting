489135b58257bce0e361c3dddf114be908ec59b3
shevakuilin <shevakuilin@gmail.com>
2019-06-10 17:25:00 +0800

    Modify project header comment

--- Example/Example/NextViewController.swift
+++ Example/Example/NextViewController.swift
@@ -1,10 +1,19 @@
 //
-//  NextViewController.swift
-//  Example
+//  Nesting
 //
-//  Created by git burning on 2019/5/26.
-//  Copyright © 2019 Warpdrives. All rights reserved.
+//  Copyright (c) 2019-Present Warpdrives Team - https://github.com/warpdrives
 //
+//  The software is available under the Apache 2.0 license. See the LICENSE file
+//  for more info.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
 
 import UIKit
 
