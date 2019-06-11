74ec54a490f928af8f80c6cd03ad3cf6f72209b6
shevakuilin <shevakuilin@gmail.com>
2019-06-11 16:59:54 +0800

    Creat CreatNestedProtocolModel [Migrate here]

---
+++ Source/Protocol/Model/CreatNestedProtocolModel.swift
@@ -0,0 +1,23 @@
+//
+//  Nesting
+//
+//  Copyright (c) 2019-Present Warpdrives Team - https://github.com/warpdrives
+//
+//  The software is available under the Apache 2.0 license. See the LICENSE file
+//  for more info.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+    
+
+import UIKit
+
+public class CreatNestedProtocolModel {
+    var ne_backgroundColor: UIColor = .clear
+    var ne_currentViewController = UIViewController()
+}
