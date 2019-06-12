e836576da58f5fcf734b989017dffee94433545a
shevakuilin <shevakuilin@gmail.com>
2019-06-12 21:50:01 +0800

    Modify UIViewController+NECreat organize code

--- Source/Extension/UIViewController+NECreat.swift
+++ Source/Extension/UIViewController+NECreat.swift
@@ -22,7 +22,8 @@ extension UIViewController: NECreatNested {
     ///
     /// - Parameter childConrtoller:    The childControllers in the container.
     /// - Parameter headerView:         Head view that does not participate in the linkage between the bottom lists.
-    func ne_creatNestedContainer(_ childConrtoller: [UIViewController], _ headerView: UIView?) {
+    /// - Parameter refreshTemplate:    Refresh style template for nested view controllers. Default normal.
+    func ne_creatNestedContainer(_ childConrtoller: [UIViewController], _ headerView: UIView?, _ refreshTemplate: NERefreshTemplate = .normal) {
         ne_assert(type: .childControllerCount, value: childConrtoller.count)
         
         /// Register scroll event monitor
@@ -41,6 +42,10 @@ extension UIViewController: NECreatNested {
                                         height: screenSize.height - navigationBarHeight)
         self.view.addSubview(scrollView)
         
+        /// Add headerView
+        if let header = headerView {
+            self.view.addSubview(header)
+        }
         /// Add childController
         for i in 0..<childConrtoller.count {
             let viewController = childConrtoller[i]
@@ -50,18 +55,25 @@ extension UIViewController: NECreatNested {
                                                width: screenSize.width,
                                                height: screenSize.height)
             scrollView.addSubview(viewController.view)
-            
-            let subviews = viewController.view.subviews
-            for view in subviews {
-                /// - Note: Temporarily does not support the existence of multiple tableviews in childViewController.
-                if view.isKind(of: UITableView.classForCoder()) {
-                    NEMonitor.shared().scrollMonitor.monitor(tableView: view as! UITableView)
-                }
-            }
+            /// Monitor tableview scroll events
+            ne_monitorTableView(viewController: viewController, headerView: headerView, refreshTemplate: refreshTemplate)
         }
-        /// Add headerView
-        if let header = headerView {
-            self.view.addSubview(header)
+    }
+    
+    /// Monitor tableview scroll events
+    ///
+    /// - Parameter viewController:     Nested view controller's childViewController.
+    /// - Parameter headerView:         Head view that does not participate in the linkage between the bottom lists.
+    /// - Parameter refreshTemplate:    Refresh style template for nested view controllers.
+    private func ne_monitorTableView(viewController: UIViewController, headerView: UIView?, refreshTemplate: NERefreshTemplate) {
+        let subviews = viewController.view.subviews
+        for view in subviews {
+            /// - Todo: Temporarily does not support the existence of multiple tableviews in childViewController.
+            if view.isKind(of: UITableView.classForCoder()) {
+                let tableView = view as! UITableView
+                tableView.ne_setContent(headerView, refreshTemplate)
+                NEMonitor.shared().scrollMonitor.monitor(tableView: tableView)
+            }
         }
     }
 }
