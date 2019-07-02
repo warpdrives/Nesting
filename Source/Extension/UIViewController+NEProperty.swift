f36cf429933329dcb07fd49e6dd6ab5bf74cec54
shevakuilin <shevakuilin@gmail.com>
2019-07-02 16:12:10 +0800

    Modify UIViewController+NEProperty replace NSMapTable with the Associated object

--- Source/Extension/UIViewController+NEProperty.swift
+++ Source/Extension/UIViewController+NEProperty.swift
@@ -30,55 +30,84 @@ public extension UIViewController {
         }
     }
     
+    /// The keys for the association.
+    private struct NE_AssociatedKeys {
+        static var ne_scrollView: UInt8 = 0
+        static var ne_header: UInt8 = 1
+        static var ne_monitor: UInt8 = 2
+    }
+    
     /// An NSMapTable responsible for storing property.
-    private static var _mapTable = NSMapTable<NSString, AnyObject>(keyOptions: .strongMemory, valueOptions: .weakMemory)
+//    private static var _mapTable = NSMapTable<NSString, AnyObject>(keyOptions: .strongMemory, valueOptions: .weakMemory)
 
     /// Monitor the scroll events of the UITableView.
     var ne_monitor: NEMonitor {
         get {
-            let monitor: NEMonitor = UIViewController._mapTable.object(forKey: ne_map(key: "NEMonitor")) as? NEMonitor ?? NEMonitor()
-            monitor.registerScrollMonitor()
+//            let monitor: NEMonitor = UIViewController._mapTable.object(forKey: ne_map(key: "NEMonitor")) as? NEMonitor ?? NEMonitor()
+//            monitor.registerScrollMonitor()
+//            monitor.setDelegate(targrt: self)
+//            return monitor
+            let monitor: NEMonitor = ne_associatedObject(base: self, key: &NE_AssociatedKeys.ne_monitor) {
+                return NEMonitor()
+            }
+//            monitor.registerScrollMonitor()
             monitor.setDelegate(targrt: self)
             return monitor
         }
         set(newValue) {
-            UIViewController._mapTable.setObject(newValue, forKey: ne_map(key: "NEMonitor"))
+//            UIViewController._mapTable.setObject(newValue, forKey: ne_map(key: "NEMonitor"))
+            ne_associateObject(base: self, key: &NE_AssociatedKeys.ne_monitor, value: newValue)
         }
     }
     
     /// HeaderView of a nested view controller.
     var ne_header: UIView {
         get {
-            let view = UIViewController._mapTable.object(forKey: ne_map(key: "Header")) as? UIView ?? UIView()
-            return view
+//            let view = UIViewController._mapTable.object(forKey: ne_map(key: "Header")) as? UIView ?? UIView()
+//            return view
+            return ne_associatedObject(base: self, key: &NE_AssociatedKeys.ne_header, initialiser: {
+                UIView()
+            })
         }
         set(newValue) {
-            UIViewController._mapTable.setObject(newValue, forKey: ne_map(key: "Header"))
+//            UIViewController._mapTable.setObject(newValue, forKey: ne_map(key: "Header"))
+            ne_associateObject(base: self, key: &NE_AssociatedKeys.ne_header, value: newValue)
         }
     }
     
     /// The bottommost UIScrollView container of the nested view controller.
     var ne_scrollView: UIScrollView {
         get {
-            guard let scrollView = UIViewController._mapTable.object(forKey: ne_map(key: "ScrollView")) as? UIScrollView else {
+//            guard let scrollView = UIViewController._mapTable.object(forKey: ne_map(key: "ScrollView")) as? UIScrollView else {
+//                let spareScrollView = UIScrollView()
+//                spareScrollView.backgroundColor = .clear
+//                spareScrollView.isPagingEnabled = true
+//                spareScrollView.showsHorizontalScrollIndicator = false
+//                spareScrollView.showsVerticalScrollIndicator = false
+//                spareScrollView.scrollsToTop = false
+//                UIViewController._mapTable.setObject(spareScrollView, forKey: ne_map(key: "ScrollView"))
+//                return spareScrollView
+//            }
+//            return scrollView
+            return ne_associatedObject(base: self, key: &NE_AssociatedKeys.ne_scrollView, initialiser: {
                 let spareScrollView = UIScrollView()
                 spareScrollView.backgroundColor = .clear
                 spareScrollView.isPagingEnabled = true
                 spareScrollView.showsHorizontalScrollIndicator = false
                 spareScrollView.showsVerticalScrollIndicator = false
                 spareScrollView.scrollsToTop = false
-                UIViewController._mapTable.setObject(spareScrollView, forKey: ne_map(key: "ScrollView"))
+//                UIViewController._mapTable.setObject(spareScrollView, forKey: ne_map(key: "ScrollView"))
                 return spareScrollView
-            }
-            return scrollView
+            })
         }
         set(newValue) {
-            UIViewController._mapTable.setObject(newValue, forKey: ne_map(key: "ScrollView"))
+//            UIViewController._mapTable.setObject(newValue, forKey: ne_map(key: "ScrollView"))
+            ne_associateObject(base: self, key: &NE_AssociatedKeys.ne_scrollView, value: newValue)
         }
     }
     
     /// Generate a key of type NSString.
-    private func ne_map(key: String) -> NSString {
-        return NSString(string: "\(key)\(ne_address(instance: self))")
-    }
+//    private func ne_map(key: String) -> NSString {
+//        return NSString(string: "\(key)\(ne_address(instance: self))")
+//    }
 }
