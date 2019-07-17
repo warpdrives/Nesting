296f50cc3e4ae88d2bb8f9063d890932bf446230
shevakuilin <shevakuilin@gmail.com>
2019-07-17 13:20:07 +0800

    Modify Example add Note

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -57,6 +57,7 @@ private extension ViewController {
         /// - Note: When your categoryView does NOT exist, you can ignore.
         headerView.ne_categoryHeight = categoryView.frame.size.height
         /// Step 2: Creat NestedViewController.
+        /// - Note: ⌘ + Click [Jump to Definition] to learn more.
         self.ne_creatNestedContainer(childControllers, headerView) { (offset) in
             /// Step 3: Link with your categoryView if needed.
             categoryView.linkageCategoryTitle(offsetValue: offset.x)
